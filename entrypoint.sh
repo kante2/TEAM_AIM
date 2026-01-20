3c39976bed6564bb84ca7f04e2e4dfaf8611deac
kante2 <gangtae9050@gmail.com>
2026-01-20 14:59:09 +0900

    robot sh version update

--- entrypoint.sh
+++ entrypoint.sh
@@ -52,11 +52,13 @@ elif [ "$PROBLEM_ID" = "4" ]; then
 # mission_1_1
 elif [ "$PROBLEM_ID" = "1" ]; then
     if [ "$ROLE" = "simulator" ]; then
-        export ROS_DOMAIN_ID=106
+        # export ROS_DOMAIN_ID=106
+        export ROS_DOMAIN_ID=32
         cd /root/TEAM_AIM/Mobility_Challenge_Simulator
         ros2 launch simulator_launch simulator_launch.py
     elif [ "$ROLE" = "cav" ]; then
-        export ROS_DOMAIN_ID=106
+        # export ROS_DOMAIN_ID=106
+        export ROS_DOMAIN_ID=32
         export CAV_ID=$CAV_ID
         ros2 run mission_1_1 control_cav_mission_1_1
     else
--- mission_1_1.sh
+++ mission_1_1.sh
@@ -7,9 +7,10 @@ source install/setup.bash
 
 # Launch all mission_3 nodes in separate terminals (gnome-terminal or xterm)
 # Simulator
-( PROBLEM_ID=1 ROLE=simulator ./entrypoint.sh ) &
+# ( PROBLEM_ID=1 ROLE=simulator ./entrypoint.sh ) &
 
 # CAVs
-( PROBLEM_ID=1 ROLE=cav CAV_ID=1 ./entrypoint.sh )
+# ( PROBLEM_ID=1 ROLE=cav CAV_ID=1 ./entrypoint.sh )
+( PROBLEM_ID=1 ROLE=cav CAV_ID=32 ./entrypoint.sh )
 
 wait
