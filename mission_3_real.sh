e16a3c6c3344a1823a8beda9d3df6d45b857c99c
kante2 <gangtae9050@gmail.com>
2026-02-08 21:25:39 +0900

    FIX real_cav_ids

--- mission_3_real.sh
+++ mission_3_real.sh
@@ -19,16 +19,16 @@ source install/setup.bash
 export TEAM_AIM_HOME="/home/aim/TEAM_AIM"
 
 # Four CAVs REAL ROBOT
-# 32, 5, 9, 10
-( PROBLEM_ID=4 ROLE=cav CAV_ID=32 CAV_IDS="32,5,9,10" TEAM_AIM_HOME="/home/aim/TEAM_AIM" ./entrypoint.sh ) & # path_mission3_01.csv
-( PROBLEM_ID=4 ROLE=cav CAV_ID=5 CAV_IDS="32,5,9,10" TEAM_AIM_HOME="/home/aim/TEAM_AIM" ./entrypoint.sh ) & # path_mission3_02.csv
-( PROBLEM_ID=4 ROLE=cav CAV_ID=9 CAV_IDS="32,5,9,10" TEAM_AIM_HOME="/home/aim/TEAM_AIM" ./entrypoint.sh ) & # path_mission3_03.csv
-( PROBLEM_ID=4 ROLE=cav CAV_ID=10 CAV_IDS="32,5,9,10" TEAM_AIM_HOME="/home/aim/TEAM_AIM" ./entrypoint.sh ) & # path_mission3_04.csv
+# 1,2,3,4
+( PROBLEM_ID=4 ROLE=cav CAV_ID=1 CAV_IDS="1,2,3,4" TEAM_AIM_HOME="/home/aim/TEAM_AIM" ./entrypoint.sh ) & # path_mission3_01.csv
+( PROBLEM_ID=4 ROLE=cav CAV_ID=2 CAV_IDS="1,2,3,4" TEAM_AIM_HOME="/home/aim/TEAM_AIM" ./entrypoint.sh ) & # path_mission3_02.csv
+( PROBLEM_ID=4 ROLE=cav CAV_ID=3 CAV_IDS="1,2,3,4" TEAM_AIM_HOME="/home/aim/TEAM_AIM" ./entrypoint.sh ) & # path_mission3_03.csv
+( PROBLEM_ID=4 ROLE=cav CAV_ID=4 CAV_IDS="1,2,3,4" TEAM_AIM_HOME="/home/aim/TEAM_AIM" ./entrypoint.sh ) & # path_mission3_04.csv
 
 # Tower
-( PROBLEM_ID=4 ROLE=tower CAV_IDS="32,5,9,10" TEAM_AIM_HOME="/home/aim/TEAM_AIM" ./entrypoint.sh ) &
+( PROBLEM_ID=4 ROLE=tower CAV_IDS="1,2,3,4" TEAM_AIM_HOME="/home/aim/TEAM_AIM" ./entrypoint.sh ) &
 
 # Rotary
-( PROBLEM_ID=4 ROLE=rotary CAV_IDS="32,5,9,10" TEAM_AIM_HOME="/home/aim/TEAM_AIM" ./entrypoint.sh ) &
+( PROBLEM_ID=4 ROLE=rotary CAV_IDS="1,2,3,4" TEAM_AIM_HOME="/home/aim/TEAM_AIM" ./entrypoint.sh ) &
 
 wait
